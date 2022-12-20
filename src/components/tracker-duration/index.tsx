5f3d25a5d4e7dc0c2eb1856a605a4b763fb73e82
Lorenzo Bonatti <imlorenzobonatti@gmail.com>
2022-12-21 00:02:11 +0100

    feat: integrate tracker duratin label into a dedicated component

--- src/components/tracker-duration/index.tsx
+++ src/components/tracker-duration/index.tsx
@@ -1,5 +1,35 @@
-export const TrackerDuration = (props: TrackerDurationProps) => {
+import {useEffect, useRef, useState} from "react";
+import {Tracker, TrackerState} from "@models/index";
+import moment from "moment/moment";
+
+export const TrackerDuration = (data: Tracker): string => {
+
+    // Timer
+    const timer = useRef<NodeJS.Timer>();
+    const [duration, setDuration] = useState<string>('00:00:00');
+
+    useEffect(() => {
+        // Check if state is START and startedAt is present
+        if (data.state === TrackerState.START && data.startedAt) {
+            // Set timer interval
+            timer.current = setInterval(() => {
+                // Get duration
+                setDuration(moment.utc(moment().diff(data.startedAt)).format('HH:mm:ss'));
+            }, 1000);
+        }
+        // Clear timer on change
+        return () => {
+            // Check if timer is not null
+            if (timer.current) {
+                // Clear interval
+                clearInterval(timer.current);
+                // Clear duration
+                setDuration('00:00:00');
+            }
+        }
+    }, [data.state, data.startedAt]);
+
     return (
-        <></>
+        duration
     )
 }
