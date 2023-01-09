5ce91245b5600f3b3a5baa1b5b44d8735207d43f
Lorenzo Bonatti <imlorenzobonatti@gmail.com>
2023-01-09 23:51:37 +0100

    feat: review Dashboard for Trackers elements

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,15 +1,23 @@
-import React, {ReactElement} from "react";
-import {Trackers} from "@pages/trackers";
-import {WorkLogs} from "@pages/work-logs";
+import React, { ReactElement, useContext } from "react";
+import { TrackersView } from "@components/trackers-view";
+import { WorkLogs } from "@pages/work-logs";
+import { DashboardTrackersPagination } from "@models/index";
+import { UserSettingsContext } from "@context/user-settings";
 
 export const Dashboard = (): ReactElement => {
+
+    const userSetting = useContext(UserSettingsContext);
+
     return (
         <div className='page space-y-5'>
             <h1 className="text-xl text-primary">Dashboard</h1>
             {/* Include trackers */}
-            <Trackers/>
+            <TrackersView
+                limit={userSetting?.dashboardTrackersPagination === DashboardTrackersPagination.SEVEN ? 7 : 3}
+                userSetting={userSetting || undefined}
+            />
             {/* Include work logs */}
-            <WorkLogs/>
+            <WorkLogs />
         </div>
     );
 };
